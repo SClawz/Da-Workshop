d819ad02f12a50771c9810372f0c1c8bc0d3e050
Zane Zglobicki <zzglobicki@gmail.com>
2014-05-16 09:53:46 -0400

    Testing adding eclipse projects and txt files

---
+++ RanStuff/src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * Simple code that prints "Hello World!" in Java.
+ */
+
+/**
+ * @author OIT-Zane
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
